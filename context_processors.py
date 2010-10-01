211f6ac0a77ea1caf888318d0184c79a38f56e34
Adam Ernst <adamernst@cosmicsoft.net>
2010-10-01 16:50:01 -0400

    Reunions and images

--- context_processors.py
+++ context_processors.py
@@ -2,10 +2,10 @@ from paypal.standard.forms import PayPalPaymentsForm
 
 def class_dues(request):
     paypal_dict = {
-        "amount": "20.00",
+        "amount": "25.00",
         "item_name": "Princeton 2010 Class Dues",
         "invoice": "princeton-2010-dues",
-        "notify_url": "http://www.princeton2010.org/your-ipn-location/",
+        "notify_url": "http://www.princeton2010.org/ipn/",
         "return_url": "http://www.princeton2010.org/dues-paid/",
         "cancel_return": "http://www.princeton2010.org/dues-canceled/"
     }
--- settings.py
+++ settings.py
@@ -113,8 +113,10 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
 CMS_TEMPLATES = (
     ('onecol.html', 'one column'),
+    ('blogs.html', 'blog list'),
+    ('reunions.html', 'reunions'),
 )
 
-PAYPAL_RECEIVER_EMAIL = 'adamjernst@me.com'
+PAYPAL_RECEIVER_EMAIL = 'princetonclassof2010@gmail.com'
 PAYPAL_IMAGE = 'http://www.paypal.com/en_US/i/btn/btn_paynow_LG.gif'
 PAYPAL_SANDBOX_IMAGE = 'http://www.paypal.com/en_US/i/btn/btn_paynow_LG.gif'
