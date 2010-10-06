51465891d5dcc9719bafc88a5059b5b595da1c3a
Adam Ernst <adamjernst@gmail.com>
2010-10-06 18:05:27 -0500

    Remove invoice line, problematic

--- context_processors.py
+++ context_processors.py
@@ -4,7 +4,6 @@ def class_dues(request):
     paypal_dict = {
         "amount": "25.00",
         "item_name": "Princeton 2010 Class Dues",
-        "invoice": "princeton-2010-dues",
         "notify_url": "http://www.princeton2010.org/ipn/",
         "return_url": "http://www.princeton2010.org/dues-paid/",
         "cancel_return": "http://www.princeton2010.org/dues-canceled/"
